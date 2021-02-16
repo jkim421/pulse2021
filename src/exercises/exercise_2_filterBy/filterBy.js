24cd768ebf660a2ecc1b9ebe189d51627055c416
mcherry000 <cybermcherry000@gmail.com>
2021-02-16 01:27:10 -0500

    feat: added comments

--- src/exercises/exercise_2_filterBy/filterBy.js
+++ src/exercises/exercise_2_filterBy/filterBy.js
@@ -1,14 +1,15 @@
+// Build a Suffix trie class for string search
 class Trie {
   constructor() {
     this.root = {};
   }
+  // method to insert characters of a word in trie
   insert(word, idx) {
     let node = this.root;
-    // console.log("word->", word)
-    // console.log("idx->", idx)
+    // starting at the root
     for (let char of word) {
+      // convert data input to lowercase
       char = char.toLowerCase();
-      // console.log("CHAR->", char)
       if (!node[char]) {
         node[char] = {};
         node[char].index = [idx];
@@ -16,28 +17,18 @@ class Trie {
       node[char].index.push(idx);
       node = node[char];
     }
-
-    // if(!node.index){
-    //   node.index = []
-    // } node.index.push(idx)
-    // node.isWord = true
   }
-
+  // Method to search for matching string in trie, String does not have to be a word! It can be half a word. It will enable autocomplete and quick search.
   search(str) {
-    console.log("search str=>", str);
+    console.log("searching for word=>", str);
     let node = this.root;
-
     for (let i = 0; i < str.length; i++) {
       let char = str[i].toLowerCase();
-      //  console.log("node=>", node)
-      //  console.log("char", char)
       if (!node[char]) {
-        console.log("none");
+        console.log("none! No Match Found");
         break;
       } else {
-        // console.log(node[char].index)
         if (i === str.length - 1) {
-          // console.log(node[char].index);
           return node[char].index;
         }
       }
@@ -46,16 +37,14 @@ class Trie {
     return [];
   }
 }
-
+// Main function
 export default function filterBy(data, searchTerm, keys) {
   let trie = new Trie();
+  // create a trie of values of all the keys in a given input.
   for (let i = 0; i < data.length; i++) {
     let obj = data[i];
     for (let key in obj) {
       if (keys.includes(key)) {
-        // console.log("key->", key)
-        // console.log("value->", obj[key])
-        // console.log("i->", i)
         const arr = obj[key].split(" ");
         for (let word of arr) {
           trie.insert(word, i);
@@ -63,10 +52,11 @@ export default function filterBy(data, searchTerm, keys) {
       }
     }
   }
+  // Array of Indices(input array) which match with our required string.
   const ind = trie.search(searchTerm);
-  // console.log(ind)
   let result = {};
   let output = [];
+  // if string matches and has a list of matching indices, create a output array of unique(non-duplicate) objects
   if (ind.length) {
     for (let id of ind) {
       if (!result[id]) {
